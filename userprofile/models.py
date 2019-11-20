a7d95b8569e341c88083e3cfd46a8ab0460d0237
Francisco Javier Arceo <arceofrancisco@gmail.com>
2019-11-19 21:47:35 -0500

    updated usermodel

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    gross_annual_income = models.FloatField(min=0)
+    gross_annual_income = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.first_name + self.last_name
+        return self.first_name + " " + self.last_name
