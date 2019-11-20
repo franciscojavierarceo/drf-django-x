9467e354c6d2a5d034acb2b99dfd06aae7a26547
Francisco Javier Arceo <arceofrancisco@gmail.com>
2019-11-19 21:19:21 -0500

    added to admin

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    gross_annual_income = models.FloatField(min=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name + self.last_name
