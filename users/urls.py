87fb8cc639224cd2c4aee5ec838723b90b35f271
Francisco Javier Arceo <arceofrancisco@gmail.com>
2019-11-19 20:04:46 -0500

    removed url for account/login override

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,4 @@ from django.utils.translation import gettext_lazy as _
 urlpatterns = [
     path('', SignUpPageView.as_view(), name='account_signup_custom'), 
     path('', SignUpPageView.as_view(), name='home'), 
-    path(_('accounts/login'), SignUpPageView.as_view(), name='account_login'), 
 ]
