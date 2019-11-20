894f8707f70bf73f7b2f6f8fc3af0346120cea4b
Francisco Javier Arceo <arceofrancisco@gmail.com>
2019-11-19 21:47:54 -0500

    updated spacing

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 python manage.py migrate
-python manage.py  makemigrations
+python manage.py makemigrations
+python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@djangox.com', 'password123')" | python manage.py shell
