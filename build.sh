0ecc05043ee012fca70a68c2b8055487801956fa
Francisco Javier Arceo <arceofrancisco@gmail.com>
2019-11-19 21:53:42 -0500

    updated migrations with the correct order

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-python manage.py migrate
+python manage.py makemigrations userprofile
+python manage.py migrate userprofile
 python manage.py makemigrations
 python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@djangox.com', 'password123')" | python manage.py shell
